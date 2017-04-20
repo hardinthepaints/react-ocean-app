22104fc7032b2882a2be811c19515efcafc90c60
Xander <alexander.fehsenfeld@coloradocollege.edu>
2017-04-19 17:59:28 -0700

    Added functions to heatmap to convert graph to svg. Changed 'constants' file to json format.

--- src/heatmap/myheatmap.js
+++ src/heatmap/myheatmap.js
@@ -1,120 +1,78 @@
 /* A wrapper class for the Plotly chart 'heatmapgl'.
 Based off of the animation examples from the plotly.js websites
 */
-import { axisTemplate, updatemenus, layout, trace } from './myheatmap.constants.js';
-//import './plotly-latest.min.js';
-//import './jquery-3.1.1.min.js';
+
+import heatmapConstants from './myheatmap.constants.json'
+const {layout, trace, animationAttribs } = heatmapConstants
+
 var Plotly = require('plotly.js');
-
-
 
 export default class MyHeatmap{
     
-    constructor( div, json, width, height ){
-        
+    constructor( div, json, width, height, svgCallBack ){
+                        
         /* Bind functions which will be accessed outside of variable */
         this.initHeatmap = this.initHeatmap.bind( this )
-        this.playFrames = this.playFrames.bind( this )
-
-
-        /* define sliders */    
-        this.sliders = [{
-            pad: {t: 30},
-            currentvalue: {
-                xanchor: 'right', prefix: 'frame: ',font: {color: '#888',size: 20}
-            },
-            
-        }];
+        this.playFrame = this.playFrame.bind( this )
+        this.getImage = this.getImage.bind(this)    
     
         /* The id of the div which will contain the heatmap */
         this.div = div;
-        this.plotted = false;
-        this.ratio = 1;
-        this.trace = trace[0]
         
         /* set the height */
         layout.height = height;
         layout.width = width;
         
+        /* Initialize the heatmap and add frames*/
         this.initHeatmap(json);
-        //this.addFrames(json);
+        this.addFrames(json);
         
+        this.savedFrames = {}
+        
+        this.svgCallBack = svgCallBack;
+                
     }
-    
-    //play( frames ){
-    //    
-    //    frames = Array.apply(null, Array(10000)).map(function (_, i) {return i % frames.length ;});
-    //    
-    //    this.addFrames(frames)
-    //    //Plotly.animate(this.div, [], {mode: 'next'});
-    //    /* Begin with initial animation */
-    //    
-    //    var settings =  updatemenus[0]['buttons'][0]['args'][1]
-    //    Plotly.animate(this.div, frames, settings);
-    //  
-    //}
-    //
-    //pause(){
-    //    Plotly.animate(this.div, [], settings);
-    //}
+
     /* Initially plot the map with one frame of data */
     initHeatmap( json ){
         
         console.time("initHeatmap")
         var z = json[0].z;
-        
-        /* Initial data Data */
-        var trace = [
-            {
-                z: z,
-                /* x: json.lonp,
-                y: json.latp, */
-                /*hoverinfo:"z+text",  */          
-                type: 'heatmapgl',
-                colorscale: 'Jet',
-                opacity: 1.0,
-                reversescale: false,
-                name:'trace0',
-                connectgaps: false,
-                /* zauto:true,*/
-                zmin:0,
-                zmax:33,
-            }
-        ];
-        
-        /* set the width */
-        /* layout.width = json.ratio * layout.height;
-        layout.margin = {
-                t: 100,
-                r: 100 * json.ratio,
-                b: 100,
-                l: 100 * json.ratio,
-        } */
+                
+        var myTrace = [Object.assign({}, trace[0], {z:z, visible:true})]
+        var myLayout = Object.assign({}, layout)
         
         /* Initinally plot an empty heatmap */
-        Plotly.plot(this.div, trace, layout,  {scrollZoom: false, staticPlot:true, displayModeBar:false, showLink:false});
-    }
-        
-    playFrames( json ){
-                
-        /* Make the frames to animate */        
-        var processedFrames = [];
-        
-        const formatFrame = this.formatFrame;
-        
-        //Object.keys( json.frames ).map( function( key, index ){
-        json.map( function( frame, key ){
-          
-            processedFrames.push( formatFrame(frame, key) );
+        Plotly.plot(this.div, myTrace, myLayout,  {scrollZoom: false, staticPlot:false, displayModeBar:false, showLink:false}).catch(function(e){
+            console.log(e);
         });
-        
-        Plotly.animate(this.div, processedFrames, updatemenus[0]['buttons'][0]['args'][1]);
     }
     
-    //playFrame(frame){
-    //    Plotly.animate(this.div, [frame], updatemenus[0]['buttons'][0]['args'][1]);
-    //}
+    /* Convert plot to svg and return svg to given callback function */
+    getImage( svgCallBack, currentFrame ){
+        const {div, savedFrames} = this;
+        const graphDiv = document.getElementById( div )
+        Plotly.toImage(graphDiv, {format:"png"}).then(
+            (url) => {
+                svgCallBack(url, currentFrame);
+                }   
+                                                      
+        ).catch((err) => console.log(err));
+
+    }
     
+    /* Play a frame referenced by the given frame number */
+    playFrame(frame){
+        const {div, savedFrames} = this;
+        const graphDiv = document.getElementById( div )
+        
+        Plotly.animate(div, [frame], animationAttribs).then(() => {
+            //this.getImage(this.svgCallBack, frame)
+        })
+                
+    }
+    
+    /* Add the frames using plotly's addFrames method */
     addFrames( json ) {
         /* Make the frames to animate */        
         var processedFrames = [];
@@ -127,9 +85,11 @@ export default class MyHeatmap{
             processedFrames.push( formatFrame(frame, key) );
         });
         
-        Plotly.addFrames(this.div, processedFrames)
+        Plotly.addFrames(this.div, processedFrames).catch(function(e){
+            console.log(e);
+        });
     }
-    
+
     /* Format frame data from the server so plotly can interpret */
     formatFrame( frame, key ){
             return {
@@ -137,8 +97,19 @@ export default class MyHeatmap{
                 data: [{
                     z: frame.z,
                 }],
-                traces: [0],
+                traces:[0]
             };       
         
+    }
+    
+    /* deprecated loop all the frames using plotly's animation */
+    play( frames ){
+        
+        frames = Array.apply(null, Array(10000)).map(function (_, i) {return i % frames.length ;});
+
+        Plotly.animate(this.div, frames, animationAttribs).catch(function(err){
+            console.log(err);
+        });
+      
     }
 }
